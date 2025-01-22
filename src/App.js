31fb8c0068ff8e5a47ef2725ff4a077762285e83
uliashem <shemiakinau@gmail.com>
2025-01-22 13:53:20 +0000

    New commit

--- src/App.js
+++ src/App.js
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:5b25f6c4f0b21857a4c158fb6a926b7a367a5ad551961ba7d65c67e6c85e69e9
-size 1697
+oid sha256:f7aa512d76e8c2175ae073c00b7d32911337f790d79f740f4b8234184f5dbfc6
+size 446
---
+++ src/Menu.js
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:68fbe8276404944ec68ffe31fbd6bd267c1153fe319a083a6364eacef16c8886
+size 1384
